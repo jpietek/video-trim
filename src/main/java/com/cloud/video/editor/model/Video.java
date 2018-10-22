79678ab3edce682bc1ad55f4850989b4e96ba973
jp <jan.pietek@tellyo.com>
2018-10-22 16:06:36 +0200

    video extended from pojo

--- src/main/java/com/cloud/video/editor/model/Video.java
+++ src/main/java/com/cloud/video/editor/model/Video.java
@@ -1,7 +1,5 @@
 package com.cloud.video.editor.model;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,100 +11,32 @@ import javax.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Video {
+@EqualsAndHashCode(callSuper = false)
+public class Video extends VideoPojo {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	private String videoId;
-
-	private String name;
-	private String extension;
-	private String path;
-
-	private String thumbnailLink;
 
 	@Column(length = 512)
 	private String directContentLink;
 
 	@Column(length = 512)
 	private String webContentLink;
-	private int width;
-	private int height;
-	private long duration;
-	private Date created;
-	private Date modified;
-	private Date timeTaken;
-	private long size;
-
-	private double gpsLat;
-	private double gpsLong;
-
-	private double fps;
-	private long frameCount;
-
-	private double cutIn;
-	private double cutOut;
-
-	private int sortId;
-
-	private double speed = 1.0;
-
-	private String audioSource;
-
-	private long audioBitrate;
-	private long videoBitrate;
-
-	private String audioCodecName;
-	private String videoCodecNAme;
-
-	private String profile;
-	private String level;
-	private String pixFormat;
 
 	@ManyToOne
 	@JoinColumn
 	@JsonBackReference
 	private Compilation compilation;
 
-	public Video(VideoPojo p) {
-		this.id = p.getId();
-		this.videoId = p.getVideoId();
-		this.name = p.getName();
-		this.extension = p.getExtension();
-		this.path = p.getPath();
-		this.thumbnailLink = p.getThumbnailLink();
-		this.directContentLink = p.getDirectContentLink();
-		this.webContentLink = p.getWebContentLink();
-		this.width = p.getWidth();
-		this.height = p.getHeight();
-		this.duration = p.getDuration();
-		this.created = p.getCreated();
-		this.modified = p.getModified();
-		this.timeTaken = p.getTimeTaken();
-		this.size = p.getSize();
-		this.gpsLat = p.getGpsLat();
-		this.gpsLong = p.getGpsLong();
-		this.fps = p.getFps();
-		this.frameCount = p.getFrameCount();
-		this.cutIn = p.getCutIn();
-		this.cutOut = p.getCutOut();
-		this.sortId = p.getSortId();
-		this.speed = p.getSpeed();
-		this.audioSource = p.getAudioSource();
-		this.audioBitrate = p.getAudioBitrate();
-		this.videoBitrate = p.getVideoBitrate();
-		this.audioCodecName = p.getAudioCodecName();
-		this.videoCodecNAme = p.getVideoCodecNAme();
-		this.profile = p.getProfile();
-		this.level = p.getLevel();
-		this.pixFormat = p.getPixFormat();
-		this.compilation = p.getCompilation();
+	public static Video fromPojo(VideoPojo videoPojo) {
+		return (Video) videoPojo;
 	}
 
 	public double cutInSeconds() {
